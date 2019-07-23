faa496dbf0d7b4c6de3e89592c184efeba827936
elijah <elijah@live.cn>
2019-07-23 09:44:18 +0800

    0.0.0

--- app/src/main/java/xyz/mercs/AresAndroid/Config.kt
+++ app/src/main/java/xyz/mercs/AresAndroid/Config.kt
@@ -13,11 +13,11 @@ object Config {
                 URL ="http://wiki.mercs.xyz:8123/mock/54/"
             }
             1->{//测试
-                URL ="http://ryfdd.mercs.xyz:8897/AppWebApi/api/AppWebApi/"
+                URL =""
             }
             2->{//生产
              //   URL ="http://chk.surea.cn:9007/AppWebApi/api/AppWebApi/"
-                URL ="http://rymtg.rysjamc.com:9007/AppWebApi/api/AppWebApi/"
+                URL =""
             }
 
         }
